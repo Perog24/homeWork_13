a7d337bea74f322bc6ead24dc11262973a4569ea
Perog24 <perog24@gmail.com>
2023-10-02 16:45:15 +0300

    fix bug with git

--- routes/prodactsRouter.js
+++ routes/prodactsRouter.js
@@ -1,8 +1,5 @@
 const express = require('express');
-<<<<<<< HEAD
-=======
 const productsList = require('../goodsList');
->>>>>>> ca566a07c65b4961116192eff02f9387dcf469b1
 const validation = require('../middelware/validation');
 const { getProdList, getProdByQuery, getProdByID } = require('../controllers/getControllers');
 const {addProduct, updateProdName, deleteProduct} = require('../controllers/anotherControllers');
@@ -11,19 +8,9 @@ const router = express.Router();
 
 router.get('/',  getProdList);
 router.post('/', addProduct);
-<<<<<<< HEAD
 router.get('/filter', getProdByQuery);
 router.get('/api/:id',  validation, getProdByID);
 router.put('/api/:id', validation, updateProdName);
-=======
-
-router.get('/filter', getProdByQuery);
-
-router.get('/api/:id',  validation, getProdByID);
-
-router.put('/api/:id', validation, updateProdName);
-
->>>>>>> ca566a07c65b4961116192eff02f9387dcf469b1
 router.delete('/api/:id', validation, deleteProduct);
 
 module.exports = router;
